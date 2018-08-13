2d086d6d665674d60ccce2bd672e85bde31f7002
金炳 <hzjinbing@corp.netease.com>
2018-08-13 23:38:57 +0800

    change: 修改代码里面的graphql的地址，改为当前域名下

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import AddBook from './components/AddBook'
 
 // apollo client setup
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql'
+  uri: '/graphql'
 })
 
 class App extends Component {
